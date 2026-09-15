c1ab5b2d130a1d43e78e107d6ce26b91d60ad400
Kase <133991306+YoderLN@users.noreply.github.com>
2026-09-15 16:06:52 -0500

    Effectiveness update
    Added schema for type effectiveness and started inserting data

--- src/data/schema.sql
+++ src/data/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS Pokemon;
 DROP TABLE IF EXISTS Moves;
 DROP TABLE IF EXISTS Types;
+DROP TABLE IF EXISTS AttackEffectiveness;
 
 CREATE TABLE IF NOT EXISTS Types(
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -27,6 +28,26 @@ CREATE TABLE IF NOT EXISTS POKEMON(
     BaseSpeed INT
 );
 
+CREATE TABLE IF NOT EXISTS AttackEffectiveness(
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Type TEXT REFERENCES Types(Type) UNIQUE NOT NULL,
+    Normal FLOAT,
+    Fire FLOAT,
+    Water FLOAT,
+    Electric FLOAT,
+    Grass FLOAT,
+    Ice FLOAT,
+    Fighting FLOAT,
+    Poison FLOAT,
+    Ground FLOAT,
+    Flying FLOAT,
+    Psychic FLOAT,
+    Bug FLOAT,
+    Rock FLOAT,
+    Ghost FLOAT,
+    Dragon FLOAT
+);
+
 INSERT INTO Types(Type) VALUES('Normal'), 
 ('Fire'), 
 ('Water'), 
@@ -42,3 +63,14 @@ INSERT INTO Types(Type) VALUES('Normal'),
 ('Rock'),
 ('Ghost'),
 ('Dragon');
+
+INSERT INTO AttackEffectiveness (Type, Normal, Fire, Water, Electric, Grass, ice, Fighting, 
+Poison, Ground, Flying, Psychic, Bug, Rock, Ghost, Dragon) VALUES 
+('Normal', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .5, 0, 1),
+('Fire', 1, .5, .5, 1, 2, 2, 1, 1, 1, 1, 1, 2, .5, 1, .5),
+('Water', 1, 2, .5, 1, .5, 1, 1, 1, 2, 1, 1, 1, 2, 1, .5)
+
+
+
+
+;
