692577a08e047342cffc963210f74f6d19f46e65
Kase <133991306+YoderLN@users.noreply.github.com>
2026-09-15 15:32:56 -0500

    Schema implementation
    Now using sql schema file to handle init and future insertion of data

--- src/main/db_create.py
+++ src/main/db_create.py
@@ -8,39 +8,14 @@ def db_create():
     base_dir = Path(__file__).resolve().parents[2]
     data_dir = base_dir / "src" / "data"
     database_path = data_dir / "pokedex.db"
-
+    
+    schema_path = data_dir / "schema.sql"
     conn = sqlite3.connect(database_path)
     cursor = conn.cursor()
 
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Types(
-        TypeNumber INTEGER PRIMARY KEY,
-        Type TEXT UNIQUE
-    )
-    """)
+    with sqlite3.connect(database_path) as conn:
+        with open(schema_path, "r") as file:
+            schema = file.read()
 
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Moves(
-        MoveID INT AUTO_INCREMENT PRIMARY KEY,
-        MoveName TEXT,
-        MoveType TEXT REFERENCES Types(Type),
-        Description TEXT)
-    """)
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Pokemon(
-        ID INT PRIMARY KEY,
-        Name TEXT,
-        Type1 TEXT REFERENCES Types(Type) NOT NULL,
-        Type2 TEXT REFERENCES Types(Type),
-        BaseHP INT,
-        BaseAttack INT,
-        BaseDefense INT,
-        BaseSpecial INT,
-        BaseSpeed INT
-    )
-    """)
-
-    conn.commit()
-    conn.close()
+    conn.executescript(schema)
+   
