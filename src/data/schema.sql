6eb0da086c114b58ed5eb69bffdb7762073c080d
Kase <133991306+YoderLN@users.noreply.github.com>
2026-09-15 16:11:49 -0500

    Update schema.sql

--- src/data/schema.sql
+++ src/data/schema.sql
@@ -64,7 +64,7 @@ INSERT INTO Types(Type) VALUES('Normal'),
 ('Ghost'),
 ('Dragon');
 
-INSERT INTO AttackEffectiveness (Type, Normal, Fire, Water, Electric, Grass, ice, Fighting, 
+INSERT INTO AttackEffectiveness (Type, Normal, Fire, Water, Electric, Grass, Ice, Fighting, 
 Poison, Ground, Flying, Psychic, Bug, Rock, Ghost, Dragon) VALUES 
 ('Normal', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .5, 0, 1),
 ('Fire', 1, .5, .5, 1, 2, 2, 1, 1, 1, 1, 1, 2, .5, 1, .5),
